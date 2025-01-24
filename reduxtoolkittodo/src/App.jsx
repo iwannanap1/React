22ab60e237669d2b4366cf6f89e0ac3c3fea797b
Shrijana <shrijanabudhathoki51@gmail.com>
2025-01-24 20:01:09 +0545

    reduc toolkit

---
+++ reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
