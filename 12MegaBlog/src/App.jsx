b18a0f0d2be3bd6cd3571158d93495d45eed55c9
Shrijana <shrijanabudhathoki51@gmail.com>
2025-01-28 19:04:12 +0545

    blog

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { useEffect } from "react";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/header/Header";
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        <main>
+         TODO:  {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (null)
+}
+
+export default App;
